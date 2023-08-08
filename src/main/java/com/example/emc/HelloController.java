e87cbb047c3583e14390f10ac3edf9dd518f0b62
Ruslan Satybaev <medtehnik850@gmail.com>
2023-08-08 12:32:36 +0700

    My commit

---
+++ src/main/java/com/example/emc/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.emc;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Test http get request. Hello World";
+    }
+}
